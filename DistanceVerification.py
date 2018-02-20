b9643cbc7a2323125dc90a23779fcbb7578d705b
James-AIIP-2018 <35741490+James-AIIP-2018@users.noreply.github.com>
2018-02-20 14:48:32 +0200

    Distance Verification with alerts

--- DistanceVerification.py
+++ DistanceVerification.py
@@ -22,4 +22,19 @@ with open("at_distance_sensors.csv", "rU") as dataFile:
             if row[2] == '':
                 row[2] = 0
             temp = [str(row[0]), float(row[1]), float(row[2])]
+            distanceSensors.append(temp)
         rowCount = rowCount +1
+        
+
+
+totalDistance = 1011.79591667
+cumulativeDistance = 0
+for items in distanceSensors:
+    cumulativeDistance = cumulativeDistance + items[1]
+    #print(cumulativeDistance + items[2])
+    temp = cumulativeDistance + items[2]
+    print(str(temp)+ " "+ str(totalDistance))
+    if (temp - totalDistance) == 0:
+        print("Sensor: "+items[0]+", distance verification failed.")
+   
+    
