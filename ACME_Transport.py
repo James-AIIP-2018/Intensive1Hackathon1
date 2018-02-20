051470d389cadc92cd9e3eb3584bbbd2fb17b149
James-AIIP-2018 <35741490+James-AIIP-2018@users.noreply.github.com>
2018-02-20 16:13:20 +0200

    Submission Commit

--- ACME_Transport.py
+++ ACME_Transport.py
@@ -6,34 +6,53 @@ Created on 20 Feb 2018
 import math
 import csv
 
-# List initialisation
+# Dataset list initialisation
 distanceSensors = []
     
 # Open a csv file containing distance data
 with open("at_distance_sensors.csv", "rU") as dataFile:
     
     rowCount=0
-    # Read the dataset into a list
+    # Read the dataset into a list row by row
     csv_reader = csv.reader(dataFile, delimiter=',')
     for row in csv_reader:
+        
+        # Ignore the first row (headers)
         if rowCount <> 0:
+            # If this is the last row 
             if row[0] == '':
                 row[0] = 'end'
             if row[2] == '':
                 row[2] = 0
+                
+            # Write a temporary list containing row data
             temp = [str(row[0]), float(row[1]), float(row[2])]
+            
+            # Append the list (row) to the dataset list
             distanceSensors.append(temp)
+        
+        # Increment the row counter
         rowCount = rowCount +1
         
 
-
+# Define the total distance
 totalDistance = 1011.7959
+
+# Initialise the cumulative distance
 cumulativeDistance = 0
+
+# Loop through the distance dataset
 for items in distanceSensors:
     if(items[0]<>'end'):
+        
+        # Update the cumulative value to include the new delta value
         cumulativeDistance = cumulativeDistance + items[1]
-        temp = round(cumulativeDistance + items[2],4)
-        if (temp <> totalDistance):
+        
+        # Calculate the expected total distance and round to 4 decimal places 
+        expectTotalDistance = round(cumulativeDistance + items[2],4)
+        
+        
+        if (expectTotalDistance <> totalDistance):
             print("Sensor: "+items[0]+", distance verification failed.")
    
     
@@ -47,12 +66,16 @@ powerSupplySensors_Filtered = []
 with open("at_power_sensors.csv", "rU") as dataFile:
    
     rowCount=0
-    # Read the dataset into a list
+    # Read the dataset into a list row by row
     csv_reader = csv.reader(dataFile, delimiter=',')
     for row in csv_reader:
+        # Ignore the first row (headers)
         if rowCount <> 0:
+            # Write a temporary list containing row data
             temp = [str(row[0]), float(row[1])]
+            # Append the list (row) to the dataset list
             powerSupplySensors.append(temp)
+        # Increment the row counter
         rowCount = rowCount +1
 
 
@@ -70,16 +93,22 @@ formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
+# Loop through power supply dataset
 for entry in powerSupplySensors:
+    # If the power supply falls below acceptable range
     if(entry[1] < 0.995):
+        # Calculate the error (below range) and log
         error = entry[1]-0.995
         logger.info("SENSOR ERROR: Sensor: "+entry[0]+", Current power supply error: "+str(error))      
+        # Filter below values to 0.9975 and add to filtered list
         temp = [entry[0], 0.9975]
         powerSupplySensors_Filtered.append(temp)
     else:
+        # Append valid power supply to filtered list
         temp = [entry[0], entry[1]]
         powerSupplySensors_Filtered.append(temp)
         
+    # Print Early warning
     if(entry[1] < 0.996):
         print("EARLY WARNING: Power Supply: "+str(entry[0]+" is below 0.996."))
 
--- DistanceVerification.py
+++ DistanceVerification.py
@@ -27,14 +27,53 @@ with open("at_distance_sensors.csv", "rU") as dataFile:
         
 
 
-totalDistance = 1011.79591667
+totalDistance = 1011.7959
 cumulativeDistance = 0
 for items in distanceSensors:
-    cumulativeDistance = cumulativeDistance + items[1]
-    #print(cumulativeDistance + items[2])
-    temp = cumulativeDistance + items[2]
-    print(str(temp)+ " "+ str(totalDistance))
-    if (temp - totalDistance) == 0:
-        print("Sensor: "+items[0]+", distance verification failed.")
+    if(items[0]<>'end'):
+        cumulativeDistance = cumulativeDistance + items[1]
+        #print(cumulativeDistance + items[2])
+        temp = round(cumulativeDistance + items[2],4)
+        #print(str(temp)+ " "+ str(totalDistance))
+        if (temp <> totalDistance):
+            print("Sensor: "+items[0]+", distance verification failed.")
    
     
+
+
+# List initialisation
+powerSupplySensors = []
+    
+# Open a csv file containing power supply data
+with open("at_power_sensors.csv", "rU") as dataFile:
+   
+    rowCount=0
+    # Read the dataset into a list
+    csv_reader = csv.reader(dataFile, delimiter=',')
+    for row in csv_reader:
+        if rowCount <> 0:
+            temp = [str(row[0]), float(row[1])]
+            powerSupplySensors.append(temp)
+        rowCount = rowCount +1
+
+
+# Setup logging 
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Logging filename: sensorErrors.log in write mode (will overwrite existing log file)
+handler = logging.FileHandler('sensorErrors.log', 'w')
+
+# Set logging format to include the date and time, logging level and the actual logging message
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+for entry in powerSupplySensors:
+    if(entry[1] < 0.995):
+        error = entry[1]-0.995
+        logger.info("SENSOR ERROR: Sensor: "+entry[0]+", Current power supply error: "+str(error))      
+        
+        
